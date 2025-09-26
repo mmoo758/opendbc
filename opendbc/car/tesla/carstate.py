340b07c0c82db1efeabdd00d632924b02adee717
mooo758 <yccao94@gmail.com>
2025-09-26 14:10:06 +0800

    修改cp test

--- opendbc/car/tesla/carstate.py
+++ opendbc/car/tesla/carstate.py
@@ -5,29 +5,32 @@ from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.interfaces import CarStateBase
 from opendbc.car.tesla.values import DBC, CANBUS, GEAR_MAP, STEER_THRESHOLD, CAR
 
+from opendbc.sunnypilot.car.tesla.carstate_ext import CarStateExt
+
 ButtonType = structs.CarState.ButtonEvent.Type
 
 
-class CarState(CarStateBase):
+class CarState(CarStateBase, CarStateExt):
   def __init__(self, CP, CP_SP):
-    super().__init__(CP, CP_SP)
+    CarStateBase.__init__(self, CP, CP_SP)
+    CarStateExt.__init__(self, CP, CP_SP)
     self.can_define = CANDefine(DBC[CP.carFingerprint][Bus.party])
     self.shifter_values = self.can_define.dv["DI_systemStatus"]["DI_gear"]
 
-    self.autopark = False
-    self.autopark_prev = False
+    self.summon = False
+    self.summon_prev = False
     self.cruise_enabled_prev = False
 
     self.hands_on_level = 0
     self.das_control = None
 
-  def update_autopark_state(self, autopark_state: str, cruise_enabled: bool):
-    autopark_now = autopark_state in ("ACTIVE", "COMPLETE", "SELFPARK_STARTED")
-    if autopark_now and not self.autopark_prev and not self.cruise_enabled_prev:
-      self.autopark = True
-    if not autopark_now:
-      self.autopark = False
-    self.autopark_prev = autopark_now
+  def update_summon_state(self, summon_state: str, cruise_enabled: bool):
+    summon_now = summon_state in ("ACTIVE", "COMPLETE", "SELFPARK_STARTED")
+    if summon_now and not self.summon_prev and not self.cruise_enabled_prev:
+      self.summon = True
+    if not summon_now:
+      self.summon = False
+    self.summon_prev = summon_now
     self.cruise_enabled_prev = cruise_enabled
 
   def update(self, can_parsers) -> tuple[structs.CarState, structs.CarStateSP]:
@@ -70,19 +73,20 @@ class CarState(CarStateBase):
     cruise_state = self.can_define.dv["DI_state"]["DI_cruiseState"].get(int(cp_party.vl["DI_state"]["DI_cruiseState"]), None)
     speed_units = self.can_define.dv["DI_state"]["DI_speedUnits"].get(int(cp_party.vl["DI_state"]["DI_speedUnits"]), None)
 
-    autopark_state = self.can_define.dv["DI_state"]["DI_autoparkState"].get(int(cp_party.vl["DI_state"]["DI_autoparkState"]), None)
+    # DI_autoparkState is used by Summon, not autopark (which uses DAS_autopilotState = ACTIVE_AUTOPARK)
+    summon_state = self.can_define.dv["DI_state"]["DI_autoparkState"].get(int(cp_party.vl["DI_state"]["DI_autoparkState"]), None)
     cruise_enabled = cruise_state in ("ENABLED", "STANDSTILL", "OVERRIDE", "PRE_FAULT", "PRE_CANCEL")
-    self.update_autopark_state(autopark_state, cruise_enabled)
+    self.update_summon_state(summon_state, cruise_enabled)
 
     # Match panda safety cruise engaged logic
-    ret.cruiseState.enabled = cruise_enabled and not self.autopark
+    ret.cruiseState.enabled = cruise_enabled and not self.summon
     if speed_units == "KPH":
       ret.cruiseState.speed = max(cp_party.vl["DI_state"]["DI_digitalSpeed"] * CV.KPH_TO_MS, 1e-3)
     elif speed_units == "MPH":
       ret.cruiseState.speed = max(cp_party.vl["DI_state"]["DI_digitalSpeed"] * CV.MPH_TO_MS, 1e-3)
     ret.cruiseState.available = cruise_state == "STANDBY" or ret.cruiseState.enabled
     ret.cruiseState.standstill = False  # This needs to be false, since we can resume from stop without sending anything special
-    ret.standstill = cruise_state == "STANDSTILL"
+    ret.standstill = cp_party.vl["ESP_B"]["ESP_vehicleStandstillSts"] == 1
     ret.accFaulted = cruise_state == "FAULT"
 
     # Gear
@@ -108,9 +112,9 @@ class CarState(CarStateBase):
     # LKAS
     ret.stockLkas = cp_ap_party.vl["DAS_steeringControl"]["DAS_steeringControlType"] == 2  # LANE_KEEP_ASSIST
 
-    # Stock Autosteer should be off (includes FSD)
+    # Stock Autosteer should be disengaged (includes FSD)
     if self.CP.carFingerprint in (CAR.TESLA_MODEL_3, CAR.TESLA_MODEL_Y):
-      ret.invalidLkasSetting = cp_ap_party.vl["DAS_settings"]["DAS_autosteerEnabled"] != 0
+      ret.invalidLkasSetting = cp_ap_party.vl["DAS_status"]["DAS_autopilotState"] not in (0, 1, 2) # DISABLED, UNAVAILABLE, AVAILABLE
     else:
       pass
     # Buttons # ToDo: add Gap adjust button
@@ -118,11 +122,14 @@ class CarState(CarStateBase):
     # Messages needed by carcontroller
     self.das_control = copy.copy(cp_ap_party.vl["DAS_control"])
 
+    CarStateExt.update(self, ret, can_parsers)
+
     return ret, ret_sp
 
   @staticmethod
   def get_can_parsers(CP, CP_SP):
     return {
       Bus.party: CANParser(DBC[CP.carFingerprint][Bus.party], [], CANBUS.party),
-      Bus.ap_party: CANParser(DBC[CP.carFingerprint][Bus.party], [], CANBUS.autopilot_party)
+      Bus.ap_party: CANParser(DBC[CP.carFingerprint][Bus.party], [], CANBUS.autopilot_party),
+      **CarStateExt.get_parser(CP, CP_SP),
     }
--- opendbc/safety/modes/tesla.h
+++ opendbc/safety/modes/tesla.h
@@ -2,17 +2,39 @@
 
 #include "opendbc/safety/safety_declarations.h"
 
+#define TESLA_COMMON_RX_CHECKS \
+  {.msg = {{0x2b9, 2, 8, 25U, .max_counter = 7U, .ignore_quality_flag = true}, { 0 }, { 0 }}},    /* DAS_control */                                  \
+  {.msg = {{0x488, 2, 4, 50U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},   /* DAS_steeringControl */                          \
+  {.msg = {{0x257, 0, 8, 50U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},   /* DI_speed (speed in kph) */                      \
+  {.msg = {{0x155, 0, 8, 50U, .max_counter = 15U}, { 0 }, { 0 }}},                                /* ESP_B (2nd speed in kph) */                     \
+  {.msg = {{0x370, 0, 8, 100U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},  /* EPAS3S_sysStatus (steering angle) */            \
+  {.msg = {{0x118, 0, 8, 100U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},  /* DI_systemStatus (gas pedal) */                  \
+  {.msg = {{0x39d, 0, 5, 25U, .max_counter = 15U}, { 0 }, { 0 }}},                                /* IBST_status (brakes) */                         \
+  {.msg = {{0x286, 0, 8, 10U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},   /* DI_state (acc state) */                         \
+  {.msg = {{0x311, 0, 7, 10U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},   /* UI_warning (blinkers, buckle switch & doors) */ \
+
+#define TESLA_VEHICLE_BUS_ADDR_CHECK \
+  {.msg = {{0x3DF, 1, 8, 2U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},    /* UI_status2 */ \
+
 static bool tesla_longitudinal = false;
 static bool tesla_stock_aeb = false;
 
 // Only rising edges while controls are not allowed are considered for these systems:
-// TODO: Only LKAS (non-emergency) is currently supported since we've only seen it
-static bool tesla_stock_lkas = false;
-static bool tesla_stock_lkas_prev = false;
 
-// Only Summon is currently supported due to Autopark not setting Autopark state properly
-static bool tesla_autopark = false;
-static bool tesla_autopark_prev = false;
+// Car-initiated steering outside of autopilot:
+// Lane Departure Avoidance, Emergency Lane Departure Avoidance, Autopark
+static bool tesla_stock_steering_control = false;
+static bool tesla_stock_steering_control_prev = false;
+
+// Summon (includes Smart Summon)
+// Only works while car is "off" when activated
+// TODO: Fix when car is "on"
+static bool tesla_summon = false;
+static bool tesla_summon_prev = false;
+
+// Detected VEHICLE bus
+extern bool tesla_has_vehicle_bus;
+bool tesla_has_vehicle_bus = false;
 
 static uint8_t tesla_get_counter(const CANPacket_t *msg) {
 
@@ -132,22 +154,22 @@ static void tesla_rx_hook(const CANPacket_t *msg) {
       brake_pressed = (msg->data[2] & 0x03U) == 2U;
     }
 
-    // Cruise and Autopark/Summon state
+    // Cruise and Summon state
     if (msg->addr == 0x286U) {
-      // Autopark state
-      int autopark_state = (msg->data[3] >> 1) & 0x0FU;  // DI_autoparkState
-      bool tesla_autopark_now = (autopark_state == 3) ||  // ACTIVE
+      // Summon state
+      int autopark_state = (msg->data[3] >> 1) & 0x0FU;  // DI_autoparkState (used by Summon, not actually used by autopark)
+      bool tesla_summon_now = (autopark_state == 3) ||  // ACTIVE
                                 (autopark_state == 4) ||  // COMPLETE
                                 (autopark_state == 9);    // SELFPARK_STARTED
 
       // Only consider rising edges while controls are not allowed
-      if (tesla_autopark_now && !tesla_autopark_prev && !cruise_engaged_prev) {
-        tesla_autopark = true;
+      if (tesla_summon_now && !tesla_summon_prev && !cruise_engaged_prev) {
+        tesla_summon = true;
       }
-      if (!tesla_autopark_now) {
-        tesla_autopark = false;
+      if (!tesla_summon_now) {
+        tesla_summon = false;
       }
-      tesla_autopark_prev = tesla_autopark_now;
+      tesla_summon_prev = tesla_summon_now;
 
       // Cruise state
       int cruise_state = (msg->data[1] >> 4) & 0x07U;
@@ -156,10 +178,19 @@ static void tesla_rx_hook(const CANPacket_t *msg) {
                             (cruise_state == 4) ||  // OVERRIDE
                             (cruise_state == 6) ||  // PRE_FAULT
                             (cruise_state == 7);    // PRE_CANCEL
-      cruise_engaged = cruise_engaged && !tesla_autopark;
+      cruise_engaged = cruise_engaged && !tesla_summon;
 
-      vehicle_moving = cruise_state != 3; // STANDSTILL
       pcm_cruise_check(cruise_engaged);
+    }
+
+    if (msg->addr == 0x155U) {
+      vehicle_moving = !GET_BIT(msg, 41U);  // ESP_vehicleStandstillSts
+    }
+  }
+
+  if (msg->bus == 1U) {
+    if (msg->addr == 0x3DFU) {
+      mads_button_press = (msg->data[3] == 3U) ? MADS_BUTTON_PRESSED : MADS_BUTTON_NOT_PRESSED;
     }
   }
 
@@ -173,16 +204,16 @@ static void tesla_rx_hook(const CANPacket_t *msg) {
     // DAS_steeringControl
     if (msg->addr == 0x488U) {
       int steering_control_type = msg->data[2] >> 6;
-      bool tesla_stock_lkas_now = steering_control_type == 2;  // "LANE_KEEP_ASSIST"
+      bool tesla_stock_steering_control_now = steering_control_type != 0;  // "NONE"
 
       // Only consider rising edges while controls are not allowed
-      if (tesla_stock_lkas_now && !tesla_stock_lkas_prev && !controls_allowed && !m_mads_state.system_enabled) {
-        tesla_stock_lkas = true;
+      if (tesla_stock_steering_control_now && !tesla_stock_steering_control_prev && !is_lat_active()) {
+        tesla_stock_steering_control = true;
       }
-      if (!tesla_stock_lkas_now) {
-        tesla_stock_lkas = false;
+      if (!tesla_stock_steering_control_now) {
+        tesla_stock_steering_control = false;
       }
-      tesla_stock_lkas_prev = tesla_stock_lkas_now;
+      tesla_stock_steering_control_prev = tesla_stock_steering_control_now;
     }
   }
 }
@@ -212,7 +243,7 @@ static bool tesla_tx_hook(const CANPacket_t *msg) {
   bool violation = false;
 
   // Don't send any messages when Autopark is active
-  if (tesla_autopark) {
+  if (tesla_summon) {
     violation = true;
   }
 
@@ -236,7 +267,7 @@ static bool tesla_tx_hook(const CANPacket_t *msg) {
       violation = true;
     }
 
-    if (tesla_stock_lkas) {
+    if (tesla_stock_steering_control) {
       // Don't allow any steering commands when stock LKAS is active
       violation = true;
     }
@@ -292,14 +323,14 @@ static bool tesla_fwd_hook(int bus_num, int addr) {
   bool block_msg = false;
 
   if (bus_num == 2) {
-    if (!tesla_autopark) {
+    if (!tesla_summon) {
       // APS_eacMonitor
       if (addr == 0x27d) {
         block_msg = true;
       }
 
       // DAS_steeringControl
-      if ((addr == 0x488) && !tesla_stock_lkas) {
+      if ((addr == 0x488) && !tesla_stock_steering_control) {
         block_msg = true;
       }
 
@@ -333,31 +364,38 @@ static safety_config tesla_init(uint16_t param) {
   tesla_longitudinal = GET_FLAG(param, TESLA_FLAG_LONGITUDINAL_CONTROL);
 #endif
 
+  const int TESLA_PARAM_SP_VEHICLE_BUS = 1;
+
+  tesla_has_vehicle_bus = GET_FLAG(current_safety_param_sp, TESLA_PARAM_SP_VEHICLE_BUS);
+
   tesla_stock_aeb = false;
-  tesla_stock_lkas = false;
-  tesla_stock_lkas_prev = false;
+  tesla_stock_steering_control = false;
+  tesla_stock_steering_control_prev = false;
   // we need to assume Autopark/Summon on startup since DI_state is a low freq msg.
   // this is so that we don't fault if starting while these systems are active
-  tesla_autopark = true;
-  tesla_autopark_prev = false;
+  tesla_summon = true;
+  tesla_summon_prev = false;
 
   static RxCheck tesla_model3_y_rx_checks[] = {
-    {.msg = {{0x2b9, 2, 8, 25U, .max_counter = 7U, .ignore_quality_flag = true}, { 0 }, { 0 }}},    // DAS_control
-    {.msg = {{0x488, 2, 4, 50U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},   // DAS_steeringControl
-    {.msg = {{0x257, 0, 8, 50U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},   // DI_speed (speed in kph)
-    {.msg = {{0x155, 0, 8, 50U, .max_counter = 15U}, { 0 }, { 0 }}},                                // ESP_B (2nd speed in kph)
-    {.msg = {{0x370, 0, 8, 100U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},  // EPAS3S_sysStatus (steering angle)
-    {.msg = {{0x118, 0, 8, 100U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},  // DI_systemStatus (gas pedal)
-    {.msg = {{0x39d, 0, 5, 25U, .max_counter = 15U}, { 0 }, { 0 }}},                                // IBST_status (brakes)
-    {.msg = {{0x286, 0, 8, 10U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},   // DI_state (acc state)
-    {.msg = {{0x311, 0, 7, 10U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},   // UI_warning (blinkers, buckle switch & doors)
+    TESLA_COMMON_RX_CHECKS
+  };
+
+  static RxCheck tesla_model3_y_vehicle_bus_rx_checks[] = {
+    TESLA_COMMON_RX_CHECKS
+    TESLA_VEHICLE_BUS_ADDR_CHECK
   };
 
   safety_config ret;
   if (tesla_longitudinal) {
-    ret = BUILD_SAFETY_CFG(tesla_model3_y_rx_checks, TESLA_M3_Y_LONG_TX_MSGS);
+    SET_TX_MSGS(TESLA_M3_Y_LONG_TX_MSGS, ret);
   } else {
-    ret = BUILD_SAFETY_CFG(tesla_model3_y_rx_checks, TESLA_M3_Y_TX_MSGS);
+    SET_TX_MSGS(TESLA_M3_Y_TX_MSGS, ret);
+  }
+
+  if (tesla_has_vehicle_bus) {
+    SET_RX_CHECKS(tesla_model3_y_vehicle_bus_rx_checks, ret);
+  } else {
+    SET_RX_CHECKS(tesla_model3_y_rx_checks, ret);
   }
   return ret;
 }
