c37c7b9628f29274091d98d41e2f1135f2e5c5bd
mmoo758 <yccao94@gmail.com>
2025-10-22 13:50:37 +0800

    Revert "CarControlSP: live params test

--- opendbc/sunnypilot/car/gm/values_ext.py
+++ opendbc/sunnypilot/car/gm/values_ext.py
@@ -5,3 +5,12 @@ This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
 
+from enum import IntFlag
+
+
+class GMFlagsSP(IntFlag):
+  NON_ACC = 1
+
+
+class GMSafetyFlagsSP:
+  NON_ACC = 1
