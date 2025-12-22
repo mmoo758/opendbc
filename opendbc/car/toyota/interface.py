aeb7e20e148a23475cb7824e482ddedfc00808de
mmoo758 <yccao94@gmail.com>
2025-12-22 23:10:27 +0800

    同步部分更新

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -157,19 +157,12 @@ class CarInterface(CarInterfaceBase):
     if candidate in UNSUPPORTED_DSU_CAR:
       ret.safetyParam |= ToyotaSafetyFlagsSP.UNSUPPORTED_DSU
 
-    if candidate in (CAR.TOYOTA_WILDLANDER, ):
-      stock_cp.lateralTuning.init('pid')
-      stock_cp.lateralTuning.pid.kiBP = [0.0]
-      stock_cp.lateralTuning.pid.kpBP = [0.0]
-      stock_cp.lateralTuning.pid.kpV = [0.6]
-      stock_cp.lateralTuning.pid.kiV = [0.1]
-      stock_cp.lateralTuning.pid.kf = 0.00007818594
 
     return ret
 
   @staticmethod
   def init(CP, CP_SP, can_recv, can_send, communication_control=None):
-    # disable radar if alpha longitudinal toggled on radar-ACC car
+    # disable radar if alpha longitudinal toggled on radar-ACC car without CAN filter/smartDSU
     if CP.flags & ToyotaFlags.DISABLE_RADAR.value:
       if communication_control is None:
         communication_control = bytes([uds.SERVICE_TYPE.COMMUNICATION_CONTROL, uds.CONTROL_TYPE.ENABLE_RX_DISABLE_TX, uds.MESSAGE_TYPE.NORMAL])
